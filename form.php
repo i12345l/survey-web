abe6a660e62164750d77c0a2618b44310ef221e1
Amelcya <81020752+Amelcya@users.noreply.github.com>
2021-03-22 16:41:28 +0900

    Add files via upload

--- form.php
+++ form.php
@@ -1,66 +1,24 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <title> 심리 테스트 설문조사 </title>
+    <title> 심리 테스트 설문조사 - AHSKNUE </title>
     <main charset = "utf-8"/>
     <script lang="javascript" src="javascript/main.js"></script>
     <script src="main.js">
     </script>
     <link rel="stylesheet" href="style.css" type="text/css">
     <script>
-        function test() {
-            var A = document.getElementById("Second");
-            var B = document.getElementById("second");
-            A.style.display = "none";
-            A.style.visibility = "hidden";
-            B.style.display = "block";
-            B.style.visibility = "visible";
-            
-            var Advantages = document.getElementsByClassName("Advantage");
-            for (var C of Advantages) {
-                C.style.display="block";
-                C.style.visibility="visible";
-            }
-        }
-        function question1() {
-            var A = document.getElementById("second");
-            var B = document.getElementById("third");
-            A.style.display = "none";
-            A.style.visibility = "hidden";
-            B.style.display = "block";
-            B.style.visibility = "visible";
-            
-            var Advantages = document.getElementsByClassName("Advantage");
-            for (var C of Advantages) {
-                C.style.display="block";
-                C.style.visibility="visible";
-            }
-        }
-        function question2() {
-            var A = document.getElementById("third");
-            var B = document.getElementById("fourth");
-            A.style.display = "none";
-            A.style.visibility = "hidden";
-            B.style.display = "block";
-            B.style.visibility = "visible";
-            
-            var Advantages = document.getElementsByClassName("Advantage");
-            for (var C of Advantages) {
-                C.style.display="block";
-                C.style.visibility="visible";
-            }
-        }
     </script>
 </head>
 <body>
 <form id = "test" action = "final.php" method = "POST">
     <span id = "Data" style = "display: block;">
         <?php
-            $a = $_POST["Name"];
+            
             $b = $_POST["Bloodtype"];
             $c = $_POST["Age"];
             $d = $_POST["Gender"];
-            print('<input type = "text" name = "Name" value = "'.$a.'"/>');
+            
             print('<input type = "text" name = "Bloodtype" value = "'.$b.'"/>');
             print('<input type = "text" name = "Age" value = "'.$c.'"/>');
             print('<input type = "text" name = "Gender" value = "'.$d.'"/>');
@@ -69,6 +27,7 @@
     </span>
     <span id = "Second" style = "display: block; visibility: visible;">
         <h2>당신의 성격유형은 어떤 것 인가요?</h2>
+        <fieldset id="fieldss">
         <input type = "radio" name = "Personality" value = "ISTJ"/> ISTJ - 청렴결백한 논리주의자 <br/>
         <input type = "radio" name = "Personality" value = "ISFJ"/> ISFJ - 용감한 수호자 <br/>
         <input type = "radio" name = "Personality" value = "INFJ"/> INFJ - 선의의 옹호자 <br/>
@@ -88,30 +47,30 @@
         <input type = "radio" name = "Personality" value = "ESFJ"/> ESFJ - 사교적인 외교관 <br/>
         <input type = "radio" name = "Personality" value = "ENFJ"/> ENFJ - 정의로운 사회운동가 <br/>
         <input type = "radio" name = "Personality" value = "ENTJ"/> ENTJ - 대담한 통솔자 <br/>
-
-        <button type = "button" class = "next" onclick = "javascript: test(),mbti();">Next</button>
+        </fieldset>
+        <button type = "button" class = "next" onclick = "javascript: insert(),mbti()">Next</button>
     </span>
     <div id = "second" style = "visibility: hidden; display: none;">
-        <h2> 해당 성격유형의 장점입니다. <br/> 현재의 당신과 얼마나 일치한가요</h2>
+        <h2> 해당 성격유형의 첫번째 특징입니다. <br/> 현재의 당신과 얼마나 일치한가요</h2>
         <table id = "show">
             <tr class = "B">
-                    <p>장점</p>
+                    <p>특징</p>
             </tr>
             <tr>
-                <h1>내용</h1>
+                <p id="q1">내용</p> 
             </tr>
         </table>
         <table>
             <tr>
-                <td> <input type="radio" name="advantage" value="correct"/>일치한다. <br/></td>
-                <td> <input type="radio" name="advantage" value="midium"/>보통이다. <br/></td>
-                <td> <input type="radio" name="advantage" value="incorrect">일치하지 않는다. <br/></td>
+                <td> <input type="radio" name="advantage" value="1"/>일치한다. <br/></td>
+                <td> <input type="radio" name="advantage" value="0"/>잘 모르겠다. <br/></td>
+                <td> <input type="radio" name="advantage" value="-1">일치하지 않는다. <br/></td>
             </tr>
         </table>
         <input type = "button" id = "next" onclick = "javascript: question1()"/>next</button>
     </div>
     <div id = "third" style = "visibility: hidden; display: none;">
-        <h2> 해당 성격유형 단점입니다. <br/> 현재의 당신과 얼마나 일치한가요</h2>
+        <h2> 해당 성격유형의 두번째 특징입니다. <br/> 현재의 당신과 얼마나 일치한가요</h2>
         <table id = "show">
             <tr class = "B">
                 <p>단점</p>
@@ -122,9 +81,9 @@
         </table>
         <table>
             <tr>
-                <td> <input type="radio" name="weakness" value="correct"/>일치한다. <br/></td>
-                <td> <input type="radio" name="weakness" value="midium"/>보통이다. <br/></td>
-                <td> <input type="radio" name="weakness" value="incorrect">일치하지 않는다. <br/></td>
+                <td> <input type="radio" name="weakness" value="1"/>일치한다. <br/></td>
+                <td> <input type="radio" name="weakness" value="0"/>잘 모르겠다. <br/></td>
+                <td> <input type="radio" name="weakness" value="-1">일치하지 않는다. <br/></td>
             </tr>
         </table>
         <input type = "button" id = "next" onclick = "javascript: question2()"/>next</button>
@@ -141,9 +100,9 @@
         </table>
         <table>
             <tr>
-                <td> <input type="radio" name="love" value="correct"/>일치한다. <br/></td>
-                <td> <input type="radio" name="love" value="midium"/>보통이다. <br/></td>
-                <td> <input type="radio" name="love" value="incorrect">일치하지 않는다. <br/></td>
+                <td> <input type="radio" name="love" value="1"/>일치한다. <br/></td>
+                <td> <input type="radio" name="love" value="0"/>잘 모르겠다. <br/></td>
+                <td> <input type="radio" name="love" value="-1">일치하지 않는다. <br/></td>
             </tr>
         </table>
         <input type = "submit"/>
--- main.js
+++ main.js
@@ -5,8 +5,51 @@ function mbti() {
                 const mbti=document.getElementsByName("Personality")[i].value;
             }
         }
-    if(mbti=="INTP"){
-        var elements = document.getElementsByTagName("h1");
-        document.write("0번째 : " + elements[0].innerText);
+    if(mbti="INTP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+    }
+}
+
+function question1() {
+    var A = document.getElementById("second");
+    var B = document.getElementById("third");
+    A.style.display = "none";
+    A.style.visibility = "hidden";
+    B.style.display = "block";
+    B.style.visibility = "visible";
+}
+
+function insert() {
+    var A = document.getElementById("Second");
+    var B = document.getElementById("second");
+    A.style.display = "none";
+    A.style.visibility = "hidden";
+    B.style.display = "block";
+    B.style.visibility = "visible";
+    
+    var Advantages = document.getElementsByClassName("Advantage");
+    for (var C of Advantages) {
+        C.style.display="block";
+        C.style.visibility="visible";
+    }
+}
+function question2() {
+    var A = document.getElementById("third");
+    var B = document.getElementById("fourth");
+    A.style.display = "none";
+    A.style.visibility = "hidden";
+    B.style.display = "block";
+    B.style.visibility = "visible";
+    
+    var Advantages = document.getElementsByClassName("Advantage");
+    for (var C of Advantages) {
+        C.style.display="block";
+        C.style.visibility="visible";
     }
 }
