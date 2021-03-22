03b2a2861ab6a4021b57b1900531e441efbb8617
Amelcya <81020752+Amelcya@users.noreply.github.com>
2021-03-22 17:07:09 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -7,13 +7,148 @@ function mbti() {
         }
     if(mbti="INTP"){
         document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
-        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
-        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
-        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
-        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
-        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
-        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
     }
+    if(mbti="INTJ"){
+        document.getElementById("q1").innerHTML = "나는 매 상황에 이성적인 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 정확한 분석에 대한 자신감이 있는 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 창의적이고 목표지향적이며 낙천적인 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 완벽주의적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 호기심이 많은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 매우 비판적인 사람이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="INFJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="INFP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="ISTJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="ISFJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="ISTP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } if(mbti="ISFP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="ENTJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="ENTP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    }
+    if(mbti="ENFJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } 
+    if(mbti="ENFP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } 
+    if(mbti="ESTJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } 
+    if(mbti="ESFJ"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } 
+    if(mbti="ESTP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } 
+    if(mbti="ESFP"){
+        document.getElementById("q1").innerHTML = "나는 좋은 분석능력을 지녔으며 풍부한 생각을 하는 사람이다.";
+        document.getElementById("q2").innerHTML = "나는 창의적이고 독창적인 사람이다.";
+        document.getElementById("q3").innerHTML = "나는 생각이 열린 사람이다.";
+        document.getElementById("q4").innerHTML = "나는 열정적인 사람이다.";
+        document.getElementById("q5").innerHTML = "나는 관찰력이 좋은 사람이다.";
+        document.getElementById("q6").innerHTML = "나는 정직하고 직진적이다.";
+        document.getElementById("q7").innerHTML = "나는 매우 사교적인 사람이다.";
+    } 
+
 }
 
 function question1() {
@@ -26,7 +161,7 @@ function question1() {
 }
 
 function insert() {
-    var A = document.getElementById("Second");
+    var A = document.getElementById("first");
     var B = document.getElementById("second");
     A.style.display = "none";
     A.style.visibility = "hidden";
