0b9af6d78f71d21232858174bddbc321e47a131c
i12345l <66990476+i12345l@users.noreply.github.com>
2021-03-22 19:59:50 +0900

    Add files via upload

--- final.php
+++ final.php
@@ -10,9 +10,16 @@
 <body>
     <?php
         $txt=$txt."\n";
+        $check=0;
+        $total=0;
         foreach ($_POST as $Key => $Value) {
             $txt=$txt." ".$Value;
+            $check++;
+            if($check>4){
+                $total+=$Value;
+            }
         }
+        $txt=$txt." ".$total;
         $myfile = fopen("result.txt", "a");
         fwrite($myfile,$txt);
         fclose($myfile);
